68bab0a53e662b556666239cb08e644698475098
Nitin Semwal <140317834+NitinSemwal2605@users.noreply.github.com>
2025-01-03 22:41:28 +0530

    Merge Sorted Array

---
+++ STEP -03 (Array)/Hard/MergeSortedArrays.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1;
+    int j = n - 1;
+    int k = m + n - 1;
+    
+    // Compare elements from end of nums1 and nums2
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+    
+    // Copy remaining elements 
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    merge(nums1, 3, nums2, 3);
+    for (int i = 0; i < nums1.size(); i++) {
+        cout << nums1[i] << " ";
+    }
+    return 0;
+}
